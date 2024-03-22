e686b37308b16bd87e83532c2b0dae380977000e
Dylan Tredger <d.tredger@gmail.com>
2024-03-21 22:30:39 -0400

    add debug, use renamed amplifier module

--- 6_baby_crow/main.py
+++ 6_baby_crow/main.py
@@ -8,55 +8,55 @@ Modules used by owl:
 - LEDs (eyes)
 - Sensors (Light Sensor)
 - Servo (neck)
-- Speaker
+- Amplifier
 """
 
 from machine import Timer
+from time import sleep
 import _thread
 
 # Modules from lib
 from leds import Leds
 from sensors import LightSensor
 from servos import Servo
-from speaker import Speaker
+from amplifier import Amplifier
 
 
 leds = Leds(18)
 light_sensor = LightSensor(26)
 servo = Servo(22)
-speaker = Speaker(9,8,7, "audio/crow_11k.wav")
+amplifier = Amplifier(9,8,7, audio_dir="audio")
 
+DEBUG = True
 INTERVAL_MINUTES = 60
 
 def light_rotate_hoot():
     leds.fade_in()
     servo.to_top()
-    speaker.play_wav()
+    amplifier.play_wav()
+    sleep(0.5)
     servo.to_bottom()
-    speaker.play_wav()
+    amplifier.play_wav()
+    sleep(0.5)
     servo.to_midpoint()
     leds.fade_out()
 
 def timed_actions():
+    if DEBUG == True:
+        light_sensor.log_reading()
     if light_sensor.over_minimum():
         light_rotate_hoot()
     else:
         leds.flash_eyes()
-    main_timer.deinit()
-    
+
+# === Timer === #
 def start_timer(main_timer, interval_min):
     interval = 60_000 * interval_min # 60_000ms = 1 min
     main_timer.init(period=interval,
                     mode=Timer.PERIODIC,
                     callback=lambda t:timed_actions())
 
-# === Timer === #
-main_timer = Timer(-1)
-start_timer(main_timer)
-
-
-
 # === Initialization Actions ===
+start_timer(Timer(-1), INTERVAL_MINUTES)
 light_rotate_hoot()
-
 
